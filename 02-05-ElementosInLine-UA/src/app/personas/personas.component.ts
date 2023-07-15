390c1ffafd3d06fdc906e48213a110da1ad2960e
martingoesthere <martin.martin.martin.justme@gmail.com>
2023-07-15 06:53:00 -0600

    first commit

---
+++ 02-05-ElementosInLine-UA/src/app/personas/personas.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-personas',
+  template: `<h1>Listado de Personas</h1>
+    <app-persona></app-persona>
+    <app-persona></app-persona>`,
+})
+export class PersonasComponent {}
